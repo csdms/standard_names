1c9a0799454cf44995a0dd7644bdfd620707ce0f
huttone <huttone@94768a2b-2f70-4691-a8b6-288111ca8583>
2013-02-15 23:55:24 +0000

    * Move io operations into io.py

---
+++ src/standard_names/io.py
@@ -0,0 +1,50 @@
+#! /usr/bin/env python
+
+import os
+
+from standard_names import (StandardName, Collection)
+from standard_names import (wiki, yaml)
+
+def _list_to_string (lines, **kwds):
+    sorted = kwds.pop ('sorted', False)
+
+    if sorted:
+        sorted_lines = list (lines)
+        sorted_lines.sort ()
+        return os.linesep.join (sorted_lines)
+    else:
+        return os.linesep.join (lines)
+
+FORMATTERS = dict (plain=_list_to_string)
+for decorator in [wiki, yaml]:
+    FORMATTERS[decorator.__name__] = decorator (_list_to_string)
+
+def _find_unique_names (models):
+    """
+    Find unique names in a iterable of StandardNames.
+    """
+    names = Collection ()
+    for model in models:
+        try:
+            intent = model['exchange items'].keys ()
+            new_names = []
+            for key in model['exchange items']:
+                try:
+                    assert (key in ['input', 'output'])
+                except AssertionError:
+                    raise KeyError (key)
+                new_names.extend (model['exchange items'][key])
+        except AttributeError:
+            new_names = model['exchange items']
+
+        for name in new_names:
+            names.add (StandardName (name))
+
+    return names
+
+def from_model_file (file):
+    import yaml
+    models = yaml.load_all (file)
+    names = _find_unique_names (models)
+    return names
+
