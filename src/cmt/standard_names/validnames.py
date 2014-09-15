c8a789c8fbdfc857c9077e6389b389c89c7dbf62
mcflugen <mcflugen@gmail.com>
2014-09-15 13:18:38 -0600

    Added load_names_from_yaml function.
    Added
    =====
    * New function load_names_from_yaml reads names, objects, quantities,
      etc. from an opened yaml file.

--- src/cmt/standard_names/validnames.py
+++ src/cmt/standard_names/validnames.py
@@ -5,6 +5,7 @@ Collection of valid standard names.
 
 import os
 import yaml
+import types
 
 
 NAMES = set()
@@ -13,29 +14,41 @@ QUANTITIES = set()
 OPERATORS = set()
 
 
+def load_names_from_yaml(file):
+    """
+    Load valid standard names from a data base of known valid names.
+    """
+    constituents = yaml.load_all(file)
+
+    valid_clauses = {}
+    for constituent in constituents:
+        for (name, clauses) in constituent.items():
+            assert(name in ['names', 'objects', 'quantities',
+                            'operators'])
+            try:
+                valid_clauses[name] |= set(clauses)
+            except KeyError:
+                valid_clauses[name] = set(clauses)
+
+    return {
+        'names': valid_clauses.pop('names', set()),
+        'objects': valid_clauses.pop('objects', set()),
+        'quantities': valid_clauses.pop('quantities', set()),
+        'operators': valid_clauses.pop('operators', set()),
+    }
+
+
 def _load_names():
     """
     Load valid standard names from a data base of known valid names.
     """
     names_file = os.path.join(os.path.dirname(__file__),
                               'data', 'standard_names.yaml')
-    with open(names_file, 'r') as names_db:
-        constituents = yaml.load_all(names_db)
+    with open(names_file, 'r') as names_fp:
+        names = load_names_from_yaml(names_fp)
 
-        valid_clauses = {}
-        for constituent in constituents:
-            for (name, clauses) in constituent.items():
-                assert(name in ['names', 'objects', 'quantities',
-                                'operators'])
-                try:
-                    valid_clauses[name] |= set(clauses)
-                except KeyError:
-                    valid_clauses[name] = set(clauses)
-
-    globals()['NAMES'] = valid_clauses.pop('names', set())
-    globals()['OBJECTS'] = valid_clauses.pop('objects', set())
-    globals()['QUANTITIES'] = valid_clauses.pop('quantities', set())
-    globals()['OPERATORS'] = valid_clauses.pop('operators', set())
+    for name in names:
+        globals()[name.upper()] = names[name]
 
 
 _load_names()
