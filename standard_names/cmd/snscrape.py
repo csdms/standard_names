f7e9440643f5b28666996e355b4a8f3f08a4eac1
mcflugen <mcflugen@gmail.com>
2016-07-26 11:18:41 -0600

    Added doctest for snscrape.

--- standard_names/cmd/snscrape.py
+++ standard_names/cmd/snscrape.py
@@ -18,10 +18,62 @@ _AS_TXT = FORMATTERS['txt']
 _DEFAULT_SEARCH = r'\b[\w~-]+__[\w~-]+'
 
 
+def snscrape(files, with_headers=False, regex=None, format='url'):
+    """Scrape names from a URL.
+
+    Parameters
+    ----------
+    files : iterable of str
+        List of files or URL to scrape.
+    with_headers : bool, optional
+        Include headers in the output that indicate the name of the source.
+    regex : str, optional
+        A regular expression that defines what a Standard Name is.
+    format : {'url', 'plain_text'}, optional
+        The format of the target that's being scraped.
+
+    Returns
+    -------
+    str
+        The scraped names.
+
+    Examples
+    --------
+    >>> from __future__ import print_function
+    >>> from six.moves import StringIO
+    >>> import standard_names as csn
+
+    >>> file1 = StringIO(\"\"\"
+    ... A file is one name, which is air__temperature.
+    ... \"\"\")
+    >>> file2 = StringIO(\"\"\"
+    ... A file is two names: air__temperature, and water__temperature.
+    ... \"\"\")
+
+    >>> print(csn.cmd.snscrape.snscrape([file1, file2], format='plain_text'))
+    air__temperature
+    air__temperature
+    water__temperature
+    """
+    regex = regex or _DEFAULT_SEARCH
+    
+    docs = {}
+    for file_name in files:
+        docs[file_name] = scrape(file_name, regex=regex, format=format)
+
+    documents = []
+    for (name, name_list) in docs.items():
+        if with_headers:
+            heading = 'Scraped from %s' % name
+        else:
+            heading = None
+        documents.append(_AS_TXT(name_list, sorted=True, heading=heading), )
+
+    return os.linesep.join(documents)
+
+
 def main():
-    """
-    Scrape standard names from a file or URL.
-    """
+    """Scrape standard names from a file or URL."""
     import argparse
 
     parser = argparse.ArgumentParser(
@@ -39,22 +91,10 @@ def main():
 
     args = parser.parse_args()
 
-    kwds = dict(format=args.reader)
-    if args.regex:
-        kwds['regex'] = args.regex
+    names = snscrape(args.file, with_headers=not args.no_headers,
+                     regex=args.regex, format=args.reader)
 
-    docs = {}
-    for file_name in args.file:
-        docs[file_name] = scrape(file_name, **kwds)
-
-    documents = []
-    for (name, name_list) in docs.items():
-        if args.no_headers:
-            heading = None
-        else:
-            heading = 'Scraped from %s' % name
-        documents.append(_AS_TXT(name_list, sorted=True, heading=heading), )
-    print(os.linesep.join(documents))
+    print(names)
 
 
 if __name__ == '__main__':
