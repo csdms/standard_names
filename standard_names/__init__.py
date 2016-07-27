2a4346643089c9a73e7700f56e0ea9f5957e6bf4
mcflugen <mcflugen@gmail.com>
2016-07-27 15:41:40 -0600

    Removed commented-out code.

--- standard_names/__init__.py
+++ standard_names/__init__.py
@@ -4,9 +4,6 @@ from .standardname import StandardName, is_valid_name
 from .registry import NamesRegistry
 from .error import BadNameError, BadRegistryError
 
-# from .decorators import (format_as_wiki, format_as_yaml, google_doc, url,
-#                          plain_text)
-# from .io import from_model_file, scrape, FORMATTERS, SCRAPERS
 
 __version__ = '0.2.2'
 
