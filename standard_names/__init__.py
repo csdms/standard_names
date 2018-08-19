dbb9c34c42d13a4f0b07703c925da5cc68e36432
mcflugen <mcflugen@gmail.com>
2018-08-18 21:57:37 -0600

    Remove check function.

--- standard_names/__init__.py
+++ standard_names/__init__.py
@@ -5,14 +5,6 @@ from .registry import NamesRegistry
 from .error import BadNameError, BadRegistryError
 
 
-__version__ = "0.2.2"
-
-
-def check():
-    from nose.core import run
-
-    run("standard_names")
-
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
