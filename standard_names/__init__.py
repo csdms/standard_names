8f2ee3ea3837ab9304a93cc77e7ac043587c57b7
Mark Piper <mark.piper@colorado.edu>
2023-12-07 14:48:20 -0700

    Configure for zest.releaser

--- standard_names/__init__.py
+++ standard_names/__init__.py
@@ -1,9 +1,10 @@
 """The CSDMS Standard Names"""
-
+import pkg_resources
 from .error import BadNameError, BadRegistryError
 from .registry import NamesRegistry
 from .standardname import StandardName, is_valid_name
 
+__version__ = pkg_resources.get_distribution("standard-names").version
 __all__ = [
     "StandardName",
     "is_valid_name",
