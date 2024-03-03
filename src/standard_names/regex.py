248a24883b98a3526fc19545fe680136073a6c5d
mcflugen <mcflugen@gmail.com>
2024-03-02 17:24:46 -0700

    add pattern to find names within text

--- src/standard_names/regex.py
+++ src/standard_names/regex.py
@@ -19,6 +19,25 @@ STANDARD_NAME_REGEX = re.compile(
     re.VERBOSE,
 )
 
+_PATTERN = re.compile(
+    r"""
+    (?<!\w)                   # Negative look-behind for a non-word character
+    [a-z]+                    # Starts with one or more lowercase letters
+    (?:                       # Start of a non-capturing group for subsequent parts
+        [-~_]?                # Optional separator: hyphen, tilde, or underscore
+        [a-zA-Z0-9]+          # One or more alphanumeric characters
+    )*                        # Zero or more repetitions of the group
+    __                        # Double underscore separator
+    [a-z]+                    # Another lowercase word
+    (?:                       # Start of a non-capturing group for subsequent parts
+        [-~_]?                # Optional separator: hyphen, tilde, or underscore
+        [a-zA-Z0-9]+          # One or more alphanumeric characters
+    )*                        # Zero or more repetitions of the group
+    (?=\W|$)                  # Positive look-ahead for a space or end of string
+    """,
+    re.VERBOSE,
+)
+
 
 def findall(line: str) -> list[str]:
-    return STANDARD_NAME_REGEX.findall(line.strip())
+    return _PATTERN.findall(line.strip())
